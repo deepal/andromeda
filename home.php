732ece95f02ec78d0bd25c425244d92926a59d16
dpjayasekara <dpjayasekara@live.com>
2014-01-03 13:40:10 +0530

    Added Sidebar of the homepage

--- home.php
+++ home.php
@@ -18,13 +18,13 @@
     
     	<div id="header-panel">
             <nav class="navbar navbar-default nav-panel-custom" role="navigation">
-            	<a class="navbar-brand" href="">Project Portal</a> 
+            	<a class="navbar-brand logo-text" href="">Project Portal</a> 
             	<form class="navbar-form navbar-right" action="logout.php">
 	            	<div class="btn-group">
 					  <button type="button" class="btn btn-default">
 						<?php 
 							if(isset($_SESSION['user'])){
-								echo "Signed in as ".$_SESSION['user']['firstname']." ".$_SESSION['user']['lastname'];								
+								echo $_SESSION['user']['firstname']." ".$_SESSION['user']['lastname'];								
 							}
 							else{
 								session_commit();
@@ -49,13 +49,44 @@
             
         </div>
         <div id="wrapper">
-			<div id="sidebar">
-        		sdfsdfs
-	        </div>
-		        
-		    <div id="contents">
-		        sdfsdfs
-		    </div>
+			<div class="row">
+				<div id="sidebar" class="col-xs-6 col-sm-3 col-md-2 col-lg-2 sidebar-custom">
+					<div class="well">
+						<button type="button" class="btn btn-primary btn-block btn-dropdown" data-toggle="collapse" data-target="#home-link">Home</button>
+						<div id="home-link" class="collapse in">
+							<ul class="nav nav-pills nav-stacked list-group collapse-div">
+								<li><a class="btn-dropdown-item" href="#">Profile</a></li>
+								<li><a class="btn-dropdown-item" href="#">Messages</a></li>
+							</ul>
+						</div>
+						<button type="button" class="btn btn-primary btn-block btn-dropdown" data-toggle="collapse" data-target="#users-link">Users</button>
+						<div id="users-link" class="collapse in">
+							<ul class="nav nav-pills nav-stacked list-group collapse-div">
+								<li><a class="btn-dropdown-item" href="#">Profile</a></li>
+								<li><a class="btn-dropdown-item" href="#">Messages</a></li>
+							</ul>
+						</div>
+						<button type="button" class="btn btn-primary btn-block btn-dropdown" data-toggle="collapse" data-target="#projects">Projects</button>
+						<div id="projects" class="collapse in">
+							<ul class="nav nav-pills nav-stacked list-group collapse-div">
+								<li><a class="btn-dropdown-item" href="#">Profile</a></li>
+								<li><a class="btn-dropdown-item" href="#">Messages</a></li>
+							</ul>
+						</div>
+						<button type="button" class="btn btn-primary btn-block btn-dropdown" data-toggle="collapse" data-target="#projects">Projects</button>
+						<div id="projects" class="collapse in ">
+							<ul class="nav nav-pills nav-stacked list-group collapse-div">
+								<li><a class="btn-dropdown-item" href="#">Profile</a></li>
+								<li><a class="btn-dropdown-item" href="#">Messages</a></li>
+							</ul>
+						</div>
+					</div>
+		        </div>
+			        
+			    <div id="contents" class="col-xs-12 col-sm-9 col-md-10 col-lg-10 contents-custom">
+			        aaaaaaaaaa
+			    </div>
+			</div>
         </div>
 
         
